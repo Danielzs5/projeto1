c2db5cf9f63f30583568096ab460558fd1c2bec0
Danielzs5 <chiquinn10@gmail.com>
2024-09-12 10:38:02 -0300

    quarto e quinto component

---
+++ src/components/FourthComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Events = () => {
+   
+    // Evento com função
+    const handleClick = (e) => {
+        console.log(e);
+        alert("Evento com função!")}
+
+        const callNames = () => {alert("Testando")}
+           
+       
+  return (
+    <div>
+    <button onClick={() => alert("Os nomes estão em ordem alfabética, inclusive nas listas anteriores!")}>
+        Clique Aqui
+    </button>
+   
+        <div><button onClick = {handleClick}> Clique com Função</button></div>
+        <div><button onClick = {callNames}> Clique com Função</button></div>
+    </div>
+  )
+}
+
+export default Events
